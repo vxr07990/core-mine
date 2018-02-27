56b18634d6e1bc21a669aa95b13ad405bf756c6e
vxr07990 <raminivamshi09@gmail.com>
2018-02-27 09:46:01 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
 <?php
+ exec('plugin
+../gradlew clean test ',$out,$err);
+	
+    echo $out; 
+    echo $err;
 /*+**********************************************************************************
  * The contents of this file are subject to the vtiger CRM Public License Version 1.1
  * ("License"); You may not use this file except in compliance with the License
  * The Original Code is:  vtiger CRM Open Source
  * The Initial Developer of the Original Code is vtiger.
  * Portions created by vtiger are Copyright (C) vtiger.
- * All Rights Reserved.
+ //* All Rights Reserved.
  ************************************************************************************/
 
 // Configure errors to throw an `ErrorException` so they're catchable
@@ -19,6 +24,7 @@ require_once 'vtlib/Vtiger/Module.php';
 require_once 'includes/main/WebUI.php';
 require_once 'vendor/autoload.php';
 require_once 'config/database.php';
+ 
 
 if (getenv('PHP_ENV') === 'dev') {
     ini_set('display_errors', 'on');
